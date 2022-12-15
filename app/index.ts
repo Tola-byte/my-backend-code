3fc09c95c355b789aeaca2f200a7f0d51d4ea2cc
Ade Tola <adetolaisaiah17@gmail.com>
2022-12-15 21:09:46 +0100

    push

--- app/index.ts
+++ app/index.ts
@@ -5,7 +5,7 @@ import sectorModel from "./model/sectors.model";
 import User from "./model/user.model";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 7199 || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
