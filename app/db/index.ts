a8c737addfc9a168f506018d79ca0b3842eff1d0
Ade Tola <adetolaisaiah17@gmail.com>
2022-12-18 12:18:18 +0100

    updated the server portconfiguration

--- app/db/index.ts
+++ app/db/index.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-const MONGO_URI = "mongodb+srv://lucifer:liluzivert@cluster0.cqdfilj.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI;
 
 console.log(MONGO_URI);
 
