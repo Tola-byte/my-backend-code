05007498889d4c9ce9c553f3916f294da835137f
Ade Tola <adetolaisaiah17@gmail.com>
2022-12-20 09:10:32 +0100

    added writeConcern

--- app/model/sectors.model.ts
+++ app/model/sectors.model.ts
@@ -5,6 +5,10 @@ const sectorSchema = new Schema({
     type: String,
     required: true,
   },
+    writeConcern: {
+      j: true,
+      wtimeout: 1000
+    }
 });
 
 sectorSchema.methods.toJSON = function () {
