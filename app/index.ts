aa87f33cdaa363df2a64397ff7980f0b93c30cf1
Ade Tola <adetolaisaiah17@gmail.com>
2022-12-20 09:42:29 +0100

    updated

--- app/index.ts
+++ app/index.ts
@@ -56,8 +56,8 @@ app.post(
     request: CustomRequest<{
       name: string;
       agreement: boolean;
-      //sectors: string[];
-      sectors : Schema.Types.ObjectId,
+      sectors: string[];
+      //sectors : Schema.Types.ObjectId,
     }>,
     response
   ) => {
