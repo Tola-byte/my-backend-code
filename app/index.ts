b980025107c3537c029d8da5790ce15bf0cc44e4
Ade Tola <adetolaisaiah17@gmail.com>
2022-12-21 13:05:27 +0100

    updates

--- app/index.ts
+++ app/index.ts
@@ -37,9 +37,9 @@ app.get("/sectors", async (_, response) => {
 
 app.get("/user", async (request, response) => {
   try {
-    console.log(request.query.userId)
-    if (!request.query.userId) throw new Error("Invalid User found");
-    const user = await User.findById(request.query.userId).populate("sectors");
+    console.log(request.query.userID)
+    if (!request.query.userID) throw new Error("Invalid User found");
+    const user = await User.findById(request.query.userID).populate("sectors");
     console.log(user)
     if (!user) {
       throw new Error("Invalid User");
