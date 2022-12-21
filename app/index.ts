b99de8ab749e8431d34bdc7ca950851d04c5cebb
Ade Tola <adetolaisaiah17@gmail.com>
2022-12-21 16:27:06 +0100

    id

--- app/index.ts
+++ app/index.ts
@@ -37,7 +37,7 @@ app.get("/sectors", async (_, response) => {
 
 app.get("/user", async (request, response) => {
   try {
-    console.log(request.query.userId)
+    console.log(request.query.userID)
     if (!request.query.userId) throw new Error("Invalid User found");
     const user = await User.findById(request.query.userId).populate("sectors");
     console.log(user)
