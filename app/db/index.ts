cc614c2740541c55c7d1799594dd76aa35dcd9da
Ade Tola <adetolaisaiah17@gmail.com>
2022-12-21 01:06:37 +0100

    adds

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,5 +1,7 @@
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import mongoose from "mongoose";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 dotenv.config();
 
@@ -11,7 +13,7 @@ console.log(MONGO_URI);
   mongoose.set("strictQuery", false);
   mongoose.connect(MONGO_URI+"/sectors-db");
   mongoose.connection
-    .once("open" , () => {
+    .once("open" , { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } , () => {
       console.log("MongoDB Connected");
     })
     .on("error", (error) => {
